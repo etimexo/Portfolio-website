a6ca5a32427f609127211eced06be35da86bc10e
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-07-25 23:11:12 +0100

    Updating

--- src/Portfolio/Main.js
+++ src/Portfolio/Main.js
@@ -43,8 +43,43 @@ export default function Main() {
   return (
     <main>
       <nav>
+        <section className="sec-1">
+        <header className="hm">
         <div className="logo">TEO</div>
         <div className="toggler">T</div>
+        </header>
+        <section className=" row sec-2">
+        <div className="profile-pic">
+          <img src={myImage} alt="My profile"/>
+        </div>
+        </section>
+        <h4>Hello, I am</h4>
+        <h1>Elijah Obisesan</h1>
+        <h6>(Also known as GentleDove & TEO)</h6>
+        <div className="typical">
+          <span>
+            <Typical
+              loop={Infinity}
+              steps={[
+                "A front-end developer",
+                1500,
+                "A lover of tech",
+                1500,
+                "An aspiring machine learning engineer",
+                1000,
+              ]}
+            />
+          </span>
+        </div>
+        <p>
+          Being a passionate front-end developer with a keen eye
+          for creating stunning and user-friendly websites. With a focus on
+          responsive design and seamless user experiences.
+        </p>
+        <div className="profile-pic pic-m">
+          <img src={myImage} alt="My profile"/>
+        </div>
+      </section>
         <div className="side">
           <ul>
             <li className="nav-item">
@@ -75,29 +110,12 @@ export default function Main() {
           </ul>
         </div>
       </nav>
-      <section className="sec-1">
-        <h4>Hello, I am</h4>
-        <h1>Elijah Obisesan</h1>
-        <h6>(Also known as GentleDove & TEO)</h6>
-        <div className="typical">
-          <span>
-            <Typical
-              loop={Infinity}
-              steps={[
-                "A front-end developer",
-                1500,
-                "A lover of tech",
-                1500,
-                "An aspiring machine learning engineer",
-                1500,
-              ]}
-            />
-          </span>
-        </div>
-        <p>
-          Being a passionate front-end developer with a keen eye
-          for creating stunning and user-friendly websites. With a focus on
-          responsive design and seamless user experiences.
+      
+    </main>
+  );
+}
+
+
           {/* I craft digital solutions that leave a lasting impact on visitors. My proficiency in
           HTML, CSS, and JavaScript, along with expertise in popular frameworks
           like React and Bootstrap, empowers me to turn ideas into reality
@@ -109,11 +127,3 @@ I love transforming concepts into visually captivating and interactive websites 
 Don't just take my word for it - check out my portfolio to see some of the projects I've had the pleasure to work on. If you're looking to collaborate on a project or have any questions, feel free to reach out. Let's build something amazing together!"
 
 Remember, your "what I do" section should be a reflection of your personality, skills, and passion for front-end development. Keep it genuine, and it will resonate with your audience, attracting potential clients, employers, or collaborators to explore more about your work. */}
-        </p>
-        <div className="profile-pic">
-          <img src={myImage} alt="My profile"/>
-        </div>
-      </section>
-    </main>
-  );
-}
